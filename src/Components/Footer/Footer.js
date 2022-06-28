11d4ba6a50a732bde0d9c8383ecf41cb816978ea
Danylo Hrytsenko <74321926+cloudmenson@users.noreply.github.com>
2022-06-28 15:39:06 +0300

    commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,41 +1,53 @@
-import { Link } from "@chakra-ui/react";
-import { Link as ScrollLink } from "react-scroll";
-import { Flex, Spacer, Box, Text, Img } from "@chakra-ui/react";
+import {Link} from "@chakra-ui/react";
+import {Link as ScrollLink} from "react-scroll";
+import {
+  Flex,
+  Spacer,
+  Box,
+  Text,
+  Img
+} from "@chakra-ui/react";
 import Logo from "../../assets/img/logo.svg";
-import { AiOutlineArrowUp } from "react-icons/ai";
+import {AiOutlineArrowUp} from "react-icons/ai";
 import "./Footer.css";
 
 export const Footer = () => {
   return (
     <footer className="App__footer">
-      <Flex
-        sx={{ alignItems: "center", textAlign: "center", padding: "3px 0" }}
-      >
+      <Flex sx={
+        {
+          alignItems: "center",
+          textAlign: "center",
+          padding: "3px 0"
+        }
+      }>
         <Box>
           <Link>
-            <Img
-              className="App__footer-logo"
-              href="#"
+            <Img className="App__footer-logo" href="#"
               src={Logo}
-              alt="Phone Up"
-            ></Img>
+              height="100px"
+              width="150px"
+              alt="Phone Up"/>
           </Link>
         </Box>
-        <Spacer />
+        <Spacer/>
         <Box>
-          <Text fontSize={{ base: "10px", sm: "14px", lg: "16px" }}>
+          <Text fontSize={
+            {
+              base: "10px",
+              sm: "14px",
+              lg: "16px"
+            }
+          }>
             © 2022 COPYRIGHTS BY PHONE UP - ALL RIGHTS RESERVED.
           </Text>
         </Box>
         <Box>
-          <ScrollLink
-            className="App__footer-link"
-            to="header-section"
+          <ScrollLink className="App__footer-link" to="header-section"
             spy={true}
             smooth={true}
-            duration={600}
-          >
-            <AiOutlineArrowUp className="App__footer-arrow-up" />
+            duration={600}>
+            <AiOutlineArrowUp className="App__footer-arrow-up"/>
           </ScrollLink>
         </Box>
       </Flex>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
-import { Link } from "@chakra-ui/react";
-import { Link as ScrollLink } from "react-scroll";
+import {Link} from "@chakra-ui/react";
+import {Link as ScrollLink} from "react-scroll";
 import {
   Flex,
   Spacer,
@@ -9,140 +9,171 @@ import {
   Menu,
   MenuButton,
   MenuList,
-  MenuItem,
+  MenuItem
 } from "@chakra-ui/react";
-import { Breadcrumb, BreadcrumbItem } from "@chakra-ui/react";
+import {Breadcrumb, BreadcrumbItem} from "@chakra-ui/react";
 import Logo from "../../assets/img/logo.svg";
-import { IconButton } from "@chakra-ui/react";
-import { PhoneIcon, HamburgerIcon } from "@chakra-ui/icons";
+import {IconButton} from "@chakra-ui/react";
+import {PhoneIcon, HamburgerIcon} from "@chakra-ui/icons";
 import "./Header.css";
 
 export const Header = () => {
   return (
     <header className="App__header" id="header-section">
-      <Flex
-        sx={{ alignItems: "center", textAlign: "center", padding: "10px 0" }}
-      >
+      <Flex sx={
+        {
+          alignItems: "center",
+          textAlign: "center",
+          padding: "10px 0"
+        }
+      }>
         <Box>
           <Link>
-            <Img
-              width={{ base: "150px" }}
-              className="App__header-logo"
-              href="#"
+            <Img height="100px" width="150px" className="App__header-logo" href="#"
               src={Logo}
-              alt="Phone Up"
-            />
+              alt="Phone Up"/>
           </Link>
         </Box>
-        <Spacer />
-        <Box sx={{ fontWeight: "600" }} className="App__header-links">
+        <Spacer/>
+        <Box sx={
+            {fontWeight: "600"}
+          }
+          className="App__header-links">
           <Breadcrumb>
             <BreadcrumbItem>
-              <ScrollLink
-                className="App__header-link"
-                to="about-us-section"
+              <ScrollLink className="App__header-link" to="about-us-section"
                 spy={true}
                 smooth={true}
-                duration={600}
-              >
-                <Text fontSize={{ base: "15px", lg: "18px" }}>Про нас</Text>
+                duration={600}>
+                <Text fontSize={
+                  {
+                    base: "15px",
+                    lg: "18px"
+                  }
+                }>Про нас</Text>
               </ScrollLink>
             </BreadcrumbItem>
             <BreadcrumbItem>
-              <ScrollLink
-                className="App__header-link"
-                to="services-section"
+              <ScrollLink className="App__header-link" to="services-section"
                 spy={true}
                 smooth={true}
-                duration={1000}
-              >
-                <Text fontSize={{ base: "15px", lg: "18px" }}>Послуги</Text>
+                duration={1000}>
+                <Text fontSize={
+                  {
+                    base: "15px",
+                    lg: "18px"
+                  }
+                }>Послуги</Text>
               </ScrollLink>
             </BreadcrumbItem>
             <BreadcrumbItem>
-              <ScrollLink
-                className="App__header-link"
-                to="contacts-section"
+              <ScrollLink className="App__header-link" to="contacts-section"
                 spy={true}
                 smooth={true}
-                duration={1000}
-              >
-                <Text fontSize={{ base: "15px", lg: "18px" }}>Контакти</Text>
+                duration={1000}>
+                <Text fontSize={
+                  {
+                    base: "15px",
+                    lg: "18px"
+                  }
+                }>Контакти</Text>
               </ScrollLink>
             </BreadcrumbItem>
           </Breadcrumb>
         </Box>
-        <Spacer />
-        <Box
-          className="App__header-phone-number-box"
-          sx={{ fontWeight: "600" }}
-        >
-          <Link
-            fontSize={{ base: "15px", lg: "18px" }}
+        <Spacer/>
+        <Box className="App__header-phone-number-box"
+          sx={
+            {fontWeight: "600"}
+        }>
+          <Link fontSize={
+              {
+                base: "15px",
+                lg: "18px"
+              }
+            }
             className="App__header-link-number App__header-link"
-            sx={{
-              alignItems: "center",
-              display: "flex",
-              justifyContent: "center",
-              padding: "0",
-            }}
-            href="tel:+380638365582"
-          >
-            <PhoneIcon sx={{ margin: "0 6px" }} />
+            sx={
+              {
+                alignItems: "center",
+                display: "flex",
+                justifyContent: "center",
+                padding: "0"
+              }
+            }
+            href="tel:+380638365582">
+            <PhoneIcon sx={
+              {margin: "0 6px"}
+            }/>
             063-836-55-82
           </Link>
         </Box>
-        <Box
-          className="App__main-burger-menu"
-          sx={{ color: "black", marginLeft: "15px" }}
-        >
+        <Box className="App__main-burger-menu"
+          sx={
+            {
+              color: "black",
+              marginLeft: "15px"
+            }
+        }>
           <Menu>
-            <MenuButton
-              as={IconButton}
+            <MenuButton as={IconButton}
               aria-label="Options"
-              icon={<HamburgerIcon />}
-              variant="outline"
-            />
+              icon={<HamburgerIcon/>}
+              variant="outline"/>
             <MenuList>
               <MenuItem>
-                <ScrollLink
-                  className="App__header-link"
-                  to="about-us-section"
+                <ScrollLink className="App__header-link" to="about-us-section"
                   spy={true}
                   smooth={true}
-                  duration={600}
-                >
-                  <Text fontSize={{ sm: "15px", md: "15px", lg: "22px" }}>
+                  duration={600}>
+                  <Text fontSize={
+                    {
+                      sm: "15px",
+                      md: "15px",
+                      lg: "22px"
+                    }
+                  }>
                     Про нас
                   </Text>
                 </ScrollLink>
               </MenuItem>
               <MenuItem>
-                <ScrollLink
-                  className="App__header-link"
-                  to="contacts-section"
+                <ScrollLink className="App__header-link" to="contacts-section"
                   spy={true}
                   smooth={true}
-                  duration={1000}
-                >
-                  <Text fontSize={{ sm: "15px", md: "15px", lg: "22px" }}>
+                  duration={1000}>
+                  <Text fontSize={
+                    {
+                      sm: "15px",
+                      md: "15px",
+                      lg: "22px"
+                    }
+                  }>
                     Контакти
                   </Text>
                 </ScrollLink>
               </MenuItem>
               <MenuItem>
-                <Link
-                  fontSize={{ sm: "15px", md: "15px", lg: "22px" }}
+                <Link fontSize={
+                    {
+                      sm: "15px",
+                      md: "15px",
+                      lg: "22px"
+                    }
+                  }
                   className="App__header-link-number App__header-link"
-                  sx={{
-                    alignItems: "center",
-                    display: "flex",
-                    justifyContent: "center",
-                    padding: "0",
-                  }}
-                  href="tel:+380638365582"
-                >
-                  <PhoneIcon sx={{ margin: "0 6px" }} />
+                  sx={
+                    {
+                      alignItems: "center",
+                      display: "flex",
+                      justifyContent: "center",
+                      padding: "0"
+                    }
+                  }
+                  href="tel:+380638365582">
+                  <PhoneIcon sx={
+                    {margin: "0 6px"}
+                  }/>
                   063-836-55-82
                 </Link>
               </MenuItem>
