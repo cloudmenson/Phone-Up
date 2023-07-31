7695d3c8235b84ff380b61d358ce7fef157db5ec
Danylo <cloudmenson9@gmail.com>
2023-07-31 20:09:09 +0300

    Delete empty files

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { MainPage, Header, Footer } from "components";
 import { GlobalStyles } from "styles/globalStyles";
 
-// import * as Styles from "./styles";
-
 const App = () => {
   return (
     <ChakraProvider>
